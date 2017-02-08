e122dc1338c65c21a0c2689f5fb9a44230594107
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-02-08 09:43:39 +0000

    Added invalidtypeexception

---
+++ src/org/smof/exception/InvalidTypeException.java
@@ -0,0 +1,14 @@
+package org.smof.exception;
+
+import org.smof.element.field.SmofField;
+
+@SuppressWarnings("javadoc")
+public class InvalidTypeException extends Throwable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidTypeException(Class<?> invalidType, SmofField type) {
+		super("The type " + invalidType.getName() + " is not valid for a field type " + type.getAnnotClass().getName());
+	}
+
+}
