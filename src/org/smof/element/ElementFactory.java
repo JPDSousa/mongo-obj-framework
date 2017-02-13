b2c96751d7e949961589a8b60f27ec3e3948065f
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-02-13 12:16:56 +0000

    Added SmofException to ElementFactory

--- src/org/smof/element/ElementFactory.java
+++ src/org/smof/element/ElementFactory.java
@@ -1,7 +1,9 @@
 package org.smof.element;
 
+import org.smof.exception.SmofException;
+
 @SuppressWarnings("javadoc")
 public interface ElementFactory<E extends Element> {
 
-	E createElement(final ElementFieldMap map);
+	E createElement(final ElementFieldMap map) throws SmofException;
 }
