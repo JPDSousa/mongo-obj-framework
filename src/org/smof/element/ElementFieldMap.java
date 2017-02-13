7c4e0ba5e8161d75b2ee27093ce0f46a496d5898
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-02-13 12:16:35 +0000

    added put to ElementFieldMap

--- src/org/smof/element/ElementFieldMap.java
+++ src/org/smof/element/ElementFieldMap.java
@@ -15,6 +15,10 @@ public class ElementFieldMap {
 	ElementFieldMap(Map<String, Object> fieldMap) {
 		this.fieldMap = fieldMap;
 	}
+	
+	void put(String key, Object value) {
+		fieldMap.put(key, value);
+	}
 
 	public <T> T get(String fieldName, Class<T> fieldType) {
 		return  fieldType.cast(fieldMap.get(fieldName));
