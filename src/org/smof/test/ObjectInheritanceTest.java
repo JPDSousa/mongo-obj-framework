a25e936f4797fb1b6e72f29f35bc6833a14b1054
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-02-27 14:15:33 +0000

    Updated tests

--- src/org/smof/test/ObjectInheritanceTest.java
+++ src/org/smof/test/ObjectInheritanceTest.java
@@ -2,10 +2,13 @@ package org.smof.test;
 
 import static org.junit.Assert.*;
 
+import java.time.Instant;
+
 import org.bson.BsonDocument;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.smof.annnotations.SmofBuilder;
+import org.smof.annnotations.SmofDate;
 import org.smof.annnotations.SmofNumber;
 import org.smof.annnotations.SmofParam;
 import org.smof.annnotations.SmofString;
@@ -21,15 +24,21 @@ public class ObjectInheritanceTest {
 	@BeforeClass
 	public static void setUpBeforeClass() {
 		parser = new SmofParser(null);
-		parser.registerType(Ia.class);
+		parser.registerType(Ia.class, new IaFactory());
 	}
 
 	@Test
 	public void testInterface() {
 		final Ia test = new A(30l, 31, "32");
 		final BsonDocument doc = parser.toBson(test);
-		System.out.println(doc.toJson());
 		assertEquals(test, parser.fromBson(doc, Ia.class));
+	}
+	
+	@Test
+	public void testNonRegType() {
+		final B test = new B("work", Instant.now());
+		final BsonDocument doc = parser.toBson(test);
+		assertEquals(test, parser.fromBson(doc, B.class));
 	}
 	
 	private static interface Ia extends Element {
@@ -59,10 +68,7 @@ public class ObjectInheritanceTest {
 		@SmofString(name="str1")
 		private final String str1;
 
-		@SmofBuilder
-		private A(@SmofParam(name="num1") Long num1, 
-				@SmofParam(name="num2") Integer num2, 
-				@SmofParam(name="str1") String str1) {
+		private A(Long num1, Integer num2, String str1) {
 			super();
 			this.num1 = num1;
 			this.num2 = num2;
@@ -83,7 +89,96 @@ public class ObjectInheritanceTest {
 		public String getStr1() {
 			return str1;
 		}
-		
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + (int) (num1 ^ (num1 >>> 32));
+			result = prime * result + num2;
+			result = prime * result + ((str1 == null) ? 0 : str1.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			A other = (A) obj;
+			if (num1 != other.num1) {
+				return false;
+			}
+			if (num2 != other.num2) {
+				return false;
+			}
+			if (str1 == null) {
+				if (other.str1 != null) {
+					return false;
+				}
+			} else if (!str1.equals(other.str1)) {
+				return false;
+			}
+			return true;
+		}
 	}
 
+	private static class B extends AbstractElement implements Element {
+		
+		@SmofString(name="str1")
+		private final String str1;
+		@SmofDate(name="instant")
+		private final Instant instant;
+
+		@SmofBuilder
+		private B(@SmofParam(name="str1") String str1, 
+				@SmofParam(name="instant") Instant instant) {
+			this.str1 = str1;
+			this.instant = instant;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((instant == null) ? 0 : instant.hashCode());
+			result = prime * result + ((str1 == null) ? 0 : str1.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			B other = (B) obj;
+			if (instant == null) {
+				if (other.instant != null) {
+					return false;
+				}
+			} else if (!instant.equals(other.instant)) {
+				return false;
+			}
+			if (str1 == null) {
+				if (other.str1 != null) {
+					return false;
+				}
+			} else if (!str1.equals(other.str1)) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
