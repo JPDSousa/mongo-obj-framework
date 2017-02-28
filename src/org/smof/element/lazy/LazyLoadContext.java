33b131fbb2dbcacd368d18977088c3a04b533392
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-02-28 17:50:32 +0000

    Moved lazyContext to a dedicated package

--- src/org/smof/element/lazy/LazyLoadContext.java
+++ src/org/smof/element/lazy/LazyLoadContext.java
@@ -1,13 +1,19 @@
-package org.smof.element;
+package org.smof.element.lazy;
 
 import java.util.Map;
 
 import org.bson.types.ObjectId;
 import org.smof.collection.SmofDispatcher;
+import org.smof.element.Element;
 import org.smof.exception.SmofException;
 import org.smof.field.PrimaryField;
 
-class LazyLoadContext {
+@SuppressWarnings("javadoc")
+public class LazyLoadContext {
+	
+	public static LazyLoadContext create(SmofDispatcher dispatcher, Map<PrimaryField, ObjectId> references) {
+		return new LazyLoadContext(dispatcher, references);
+	}
 	
 	private static void handleError(Throwable cause) {
 		throw new SmofException(cause);
@@ -16,14 +22,14 @@ class LazyLoadContext {
 	private final SmofDispatcher dispatcher;
 	private final Map<PrimaryField, ObjectId> references;
 	
-	LazyLoadContext(SmofDispatcher dispatcher, Map<PrimaryField, ObjectId> references) { 
+	private LazyLoadContext(SmofDispatcher dispatcher, Map<PrimaryField, ObjectId> references) { 
 		super();
 		this.dispatcher = dispatcher;
 		this.references = references;
 	}
 
 	@SuppressWarnings("unchecked")
-	Object fromObjectId(ObjectId id, Class<?> elementType) {
+	public Object fromObjectId(ObjectId id, Class<?> elementType) {
 		checkValidElementClass(elementType);
 		return dispatcher.findById(id, (Class<Element>) elementType);
 	}
@@ -34,7 +40,7 @@ class LazyLoadContext {
 		}
 	}
 
-	Map<PrimaryField,ObjectId> getReferences() {
+	public Map<PrimaryField,ObjectId> getReferences() {
 		return references;
 	}
 }
