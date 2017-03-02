b562ebbb72c3a04a1aeb32bbc6b79cdf08f008fd
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-03-02 11:00:50 +0000

    Updated tests

--- src/org/smof/test/BasicSmofTest.java
+++ src/org/smof/test/BasicSmofTest.java
@@ -26,6 +26,10 @@ import org.smof.collection.Smof;
 import org.smof.element.AbstractElement;
 import org.smof.index.IndexType;
 import org.smof.parsers.SmofType;
+import org.smof.test.dataModel.Brand;
+import org.smof.test.dataModel.Guitar;
+import org.smof.test.dataModel.Model;
+import org.smof.test.dataModel.TypeGuitar;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoWriteException;
@@ -54,21 +58,25 @@ public class BasicSmofTest {
 	public final void setUp() {
 		smof.createCollection("guitars", Guitar.class);
 		smof.createCollection("brands", Brand.class);
+		smof.createCollection("models", Model.class);
 	}
 	
 	@After
 	public final void tearDown() {
 		smof.dropCollection("guitars");
 		smof.dropCollection("brands");
+		smof.dropCollection("models");
 	}
 
 	@Test
 	public void testSingleInsert() {
-		final Brand brand = new Brand(1965, "William Shakespear", 4000);
+		final Brand brand = Brand.create("Me", "Myself", "I");
+		final Model model1 = Model.create("Manhattan", 1000, brand, Arrays.asList("red", "blue"));
+		final Model model2 = Model.create("BeeGees", 5463, brand, Arrays.asList("sunburst", "ebony"));
 		final List<Guitar> guitars = new ArrayList<>();
-		guitars.add(new Guitar(brand, "GR400", Type.ELECTRIC, Tunnings.DROPD.tunning));
-		guitars.add(new Guitar(brand, "Manhattan", Type.ACOUSTIC, Tunnings.STANDARD.tunning));
-		guitars.add(new Guitar(brand, "Roxy", Type.ACOUSTIC, Tunnings.DROPC.tunning));
+		guitars.add(Guitar.create(model2, TypeGuitar.ELECTRIC, 1, 0));
+		guitars.add(Guitar.create(model1, TypeGuitar.CLASSIC, 0, 20));
+		guitars.add(Guitar.create(model1, TypeGuitar.ACOUSTIC, 0, 0));
 		
 		for(Guitar g : guitars) {
 			smof.insert(g);
@@ -77,11 +85,13 @@ public class BasicSmofTest {
 	
 	@Test
 	public void testQueryAll() {
-		final Brand brand = new Brand(1965, "William Shakespear", 4000);
+		final Brand brand = Brand.create("You");
+		final Model model1 = Model.create("Manhattan", 1000, brand, Arrays.asList("red", "blue"));
+		final Model model2 = Model.create("BeeGees", 5463, brand, Arrays.asList("sunburst", "ebony"));
 		final Map<ObjectId, Guitar> guitars = new LinkedHashMap<>();
-		final Guitar g1 = new Guitar(brand, "GR400", Type.ELECTRIC, Tunnings.DROPD.tunning); 
-		final Guitar g2 = new Guitar(brand, "Manhattan", Type.ACOUSTIC, Tunnings.STANDARD.tunning);
-		final Guitar g3 = new Guitar(brand, "Roxy", Type.ACOUSTIC, Tunnings.DROPC.tunning);
+		final Guitar g1 = Guitar.create(model2, TypeGuitar.ELECTRIC, 1, 0); 
+		final Guitar g2 = Guitar.create(model1, TypeGuitar.CLASSIC, 0, 20);
+		final Guitar g3 = Guitar.create(model1, TypeGuitar.ACOUSTIC, 0, 0);
 		guitars.put(g1.getId(), g1);
 		guitars.put(g2.getId(), g2);
 		guitars.put(g3.getId(), g3);
@@ -97,8 +107,9 @@ public class BasicSmofTest {
 
 	@Test(expected = MongoWriteException.class)
 	public void testDuplicateKey() {
-		final Brand brand = new Brand(1965, "William Shakespear", 4000);
-		final Guitar g1 = new Guitar(brand, "GR4001", Type.ELECTRIC, Tunnings.DROPD.tunning);
+		final Brand brand = Brand.create("You");
+		final Model model2 = Model.create("BeeGees", 5463, brand, Arrays.asList("sunburst", "ebony"));
+		final Guitar g1 = Guitar.create(model2, TypeGuitar.ELECTRIC, 1, 0);
 		smof.insert(g1);
 		smof.insert(g1);
 	}
@@ -108,163 +119,6 @@ public class BasicSmofTest {
 		final String name = "drop";
 		smof.createCollection(name, ToDrop.class);
 		smof.dropCollection(name);
-	}
-
-	private static enum Type {
-		CLASSIC,
-		ACOUSTIC,
-		ELECTRIC;
-	}
-
-	private static enum Tunnings {
-		STANDARD("E-A-D-G-B-E"),
-		DROPD("D-A-D-G-B-E"),
-		DROPC("C-A-D-G-B-E");
-
-		private final List<String> tunning;
-
-		private Tunnings(String strings) {
-			tunning = Arrays.asList(strings.split("-"));
-		}
-	}
-
-	@SmofIndexes({
-		@SmofIndex(key = "main", unique = true)
-	})
-	private static class Guitar extends AbstractElement {
-
-		@SmofObject(name = "brand")
-		private final Brand brand;
-
-		@SmofString(name = "model", indexKey = "main", indexType = IndexType.TEXT)
-		private final String model;
-
-		@SmofString(name = "type", indexKey = "main", indexType = IndexType.TEXT)
-		private final Type type;
-
-		@SmofArray(name = "tunning", type = SmofType.STRING)
-		private final List<String> tunning;
-
-		@SmofBuilder
-		public Guitar(@SmofParam(name = "brand") Brand brand, 
-				@SmofParam(name="model") String model, 
-				@SmofParam(name="type") Type type, 
-				@SmofParam(name="tunning") List<String> tunning) {
-			super();
-			this.model = model;
-			this.type = type;
-			this.tunning = tunning;
-			this.brand = brand;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((brand == null) ? 0 : brand.hashCode());
-			result = prime * result + ((model == null) ? 0 : model.hashCode());
-			result = prime * result + ((tunning == null) ? 0 : tunning.hashCode());
-			result = prime * result + ((type == null) ? 0 : type.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			Guitar other = (Guitar) obj;
-			if (brand == null) {
-				if (other.brand != null) {
-					return false;
-				}
-			} else if (!brand.equals(other.brand)) {
-				return false;
-			}
-			if (model == null) {
-				if (other.model != null) {
-					return false;
-				}
-			} else if (!model.equals(other.model)) {
-				return false;
-			}
-			if (tunning == null) {
-				if (other.tunning != null) {
-					return false;
-				}
-			} else if (!tunning.equals(other.tunning)) {
-				return false;
-			}
-			if (type != other.type) {
-				return false;
-			}
-			return true;
-		}
-	}
-
-	private static class Brand extends AbstractElement{
-
-		@SmofNumber(name="year")
-		private final int year;
-
-		@SmofString(name="founder")
-		private final String founder;
-
-		@SmofNumber(name="units")
-		private final int units;
-
-		@SmofBuilder
-		public Brand(@SmofParam(name="year") Integer year, 
-				@SmofParam(name="founder") String founder, 
-				@SmofParam(name="units") Integer units) {
-			this.year = year;
-			this.founder = founder;
-			this.units = units;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((founder == null) ? 0 : founder.hashCode());
-			result = prime * result + units;
-			result = prime * result + year;
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			Brand other = (Brand) obj;
-			if (founder == null) {
-				if (other.founder != null) {
-					return false;
-				}
-			} else if (!founder.equals(other.founder)) {
-				return false;
-			}
-			if (units != other.units) {
-				return false;
-			}
-			if (year != other.year) {
-				return false;
-			}
-			return true;
-		}
 	}
 
 	private static class ToDrop extends AbstractElement {
