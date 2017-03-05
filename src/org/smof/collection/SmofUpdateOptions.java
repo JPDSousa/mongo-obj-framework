ec2a51d679778838668db4918dffe22fced8ee94
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-03-05 10:19:02 +0000

    Added support for FindOneAndUpdateOptions

--- src/org/smof/collection/SmofUpdateOptions.java
+++ src/org/smof/collection/SmofUpdateOptions.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.bson.conversions.Bson;
 
 import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.ReturnDocument;
 import com.mongodb.client.model.UpdateOptions;
 
@@ -65,11 +66,44 @@ public class SmofUpdateOptions {
 		return options;
 	}
 	
+	public FindOneAndUpdateOptions toFindOneAndUpdateOptions() {
+		final FindOneAndUpdateOptions options = new FindOneAndUpdateOptions();
+		options.bypassDocumentValidation(!validateDocuments);
+		setMaxTime(options);
+		setProjection(options);
+		options.returnDocument(ret);
+		setSort(options);
+		options.upsert(upsert);
+		return options;
+	}
+
+	private void setSort(final FindOneAndUpdateOptions options) {
+		if(sort != null) {
+			options.sort(sort);
+		}
+	}
+
+	private void setProjection(final FindOneAndUpdateOptions options) {
+		if(projection != null) {
+			options.projection(projection);
+		}
+	}
+
+	private void setMaxTime(final FindOneAndUpdateOptions options) {
+		if(maxTime != null) {
+			options.maxTime(maxTime.getLeft(), maxTime.getRight());
+		}
+	}
+	
 	public void upsert(boolean upsert) {
 		this.upsert = upsert;
 	}
 	
 	public void validateDocuments(boolean validateDocuments) {
 		this.validateDocuments = validateDocuments;
+	}
+	
+	public void setReturnDocument(ReturnDocument doc) {
+		this.ret = doc;
 	}
 }
