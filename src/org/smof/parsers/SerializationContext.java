20197294fbcb683bbe6fb0e9da6a4438c6cd7c8d
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-03-12 13:02:06 +0000

    Added serialization context

---
+++ src/org/smof/parsers/SerializationContext.java
@@ -0,0 +1,32 @@
+package org.smof.parsers;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.bson.BsonValue;
+
+class SerializationContext {
+	
+	public static SerializationContext create() {
+		return new SerializationContext();
+	}
+
+	private final Map<Pair<Object, SmofType>, BsonValue> serializationStack;
+	
+	private SerializationContext() {
+		serializationStack = new LinkedHashMap<>();
+	}
+	
+	void put(Object object, SmofType type, BsonValue serializedValue) {
+		serializationStack.put(Pair.of(object, type), serializedValue);
+	}
+	
+	boolean contains(Object object, SmofType type) {
+		return serializationStack.containsKey(Pair.of(object, type));
+	}
+	
+	BsonValue get(Object object, SmofType type) {
+		return serializationStack.get(Pair.of(object, type));
+	}
+}
