234301853ab8e0d5444e30138cd236de2b464445
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-05-03 10:11:48 +0100

    Added test suite

---
+++ src/org/smof/test/AllTests.java
@@ -0,0 +1,12 @@
+package org.smof.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuppressWarnings("javadoc")
+@RunWith(Suite.class)
+@SuiteClasses({ BasicSmofTest.class, ElementTypeFactoryTests.class, ObjectInheritanceTest.class })
+public class AllTests {
+	//no code necessary
+}
