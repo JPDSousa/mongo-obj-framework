1fcfccc3ad519afbd722a4ecb4a96ece76e9b913
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-05-09 11:17:33 +0100

    Added documentation to load collection and registerSmofObject and registerSmofFactory

--- src/org/smof/collection/Smof.java
+++ src/org/smof/collection/Smof.java
@@ -3,9 +3,9 @@ package org.smof.collection;
 import java.io.Closeable;
 
 import org.bson.BsonDocument;
-
+import org.smof.annnotations.SmofBuilder;
+import org.smof.annnotations.SmofObject;
 import org.smof.element.Element;
-import org.smof.exception.SmofException;
 import org.smof.parsers.SmofParser;
 
 import com.mongodb.MongoClient;
@@ -15,14 +15,30 @@ import com.mongodb.client.MongoDatabase;
 @SuppressWarnings("javadoc")
 public class Smof implements Closeable {
 
+	/**
+	 * Creates a new smof connection.
+	 * 
+	 * @param database mongo database
+	 * @return a new smof connection
+	 * @deprecated use {@link Smof#create(String, int, String)} instead
+	 */
 	@Deprecated
 	public static Smof create(MongoDatabase database) {
 		return new Smof(null, database);
 	}
 	
-	public static Smof create(String host, int port, String database) {
+	/**
+	 * Creates a new smof connection. Use this method to initialize the api.
+	 * 
+	 * @param host mongoDB host
+	 * @param port mongoDB port
+	 * @param databaseName mongoDB database name
+	 * @return a new smof connection
+	 */
+	public static Smof create(String host, int port, String databaseName) {
 		final MongoClient client = new MongoClient(host, port);
-		return new Smof(client, client.getDatabase(database));
+		final MongoDatabase database = client.getDatabase(databaseName);
+		return new Smof(client, database);
 	}
 
 	private final MongoDatabase database;
@@ -31,6 +47,14 @@ public class Smof implements Closeable {
 	private final SmofDispatcher dispatcher;
 	private final MongoClient client;
 
+	/**
+	 * Private constructor. This constructor is only accessed through the {@link #create(String, int, String)}
+	 * method (and additionally through the deprecated {@link #create(MongoDatabase)}), which initializes
+	 * the object
+	 * 
+	 * @param client mongo client
+	 * @param database mongo database
+	 */
 	private Smof(MongoClient client, MongoDatabase database) {
 		this.client = client;
 		this.database = database;
@@ -39,19 +63,65 @@ public class Smof implements Closeable {
 		this.parser = new SmofParser(dispatcher);
 	}
 
+	/**
+	 * Loads a new collection into the API. If the database has no collection with the name passed
+	 * as parameter, one will be created. The element class (elClass) is the type mapped to this collection.
+	 * All sub-types will be implicitly mapped to this class as well.  
+	 * 
+	 * Same as using {@link #loadCollection(String, Class, CollectionOptions)} with empty {@link CollectionOptions}.
+	 * 
+	 * @param collectionName collection name on mongoDB
+	 * @param elClass element class
+	 */
 	public <T extends Element> void loadCollection(String collectionName, Class<T> elClass) {
 		loadCollection(collectionName, elClass, CollectionOptions.create());
 	}
 	
+	/**
+	 * Loads a new collection into the API. If the database has no collection with the name passed
+	 * as parameter, one will be created. The element class (elClass) is the type mapped to this collection.
+	 * All sub-types will be implicitly mapped to this class as well. Use collection options to specify additional
+	 * properties.
+	 * 
+	 * @param collectionName collection name on mongoDB
+	 * @param elClass element class
+	 * @param options collection options
+	 */
 	public <T extends Element> void loadCollection(String collectionName, Class<T> elClass, CollectionOptions<T> options) {
 		parser.registerType(elClass);
 		loadCollection(collectionName, elClass, parser, options);
 	}
 
+	/**
+	 * Loads a new collection into the API. If the database has no collection with the name passed
+	 * as parameter, one will be created. The element class (elClass) is the type mapped to this collection.
+	 * All sub-types will be implicitly mapped to this class as well. This method is useful when the element class'
+	 * {@link SmofBuilder} is located in another type (e.g. factory pattern). The factory parameter can be either an
+	 * instance of a class or even the type itself, in case the {@link SmofBuilder} is referencing a static method.
+	 * 
+	 * Same as calling {@link #loadCollection(String, Class, Object, CollectionOptions)} with empty {@link CollectionOptions}.
+	 * 
+	 * @param collectionName collection name on mongoDB
+	 * @param elClass element class
+	 * @param factory third-party object with the element class' {@link SmofBuilder}.
+	 */
 	public <T extends Element> void loadCollection(String collectionName, Class<T> elClass, Object factory) {
 		loadCollection(collectionName, elClass, factory, CollectionOptions.create());
 	}
 	
+	/**
+	 * Loads a new collection into the API. If the database has no collection with the name passed
+	 * as parameter, one will be created. The element class (elClass) is the type mapped to this collection.
+	 * All sub-types will be implicitly mapped to this class as well. This method is useful when the element class'
+	 * {@link SmofBuilder} is located in another type (e.g. factory pattern). The factory parameter can be either an
+	 * instance of a class or even the type itself, in case the {@link SmofBuilder} is referencing a static method.
+	 * Use collection options to specify additional properties.
+	 * 
+	 * @param collectionName collection name on mongoDB
+	 * @param elClass element class
+	 * @param factory third-party object with the element class' {@link SmofBuilder}.
+	 * @param options collection options
+	 */
 	public <T extends Element> void loadCollection(String collectionName, Class<T> elClass, Object factory, CollectionOptions<T> options) {
 		parser.registerType(elClass, factory);
 		loadCollection(collectionName, elClass, parser, options);
@@ -62,11 +132,29 @@ public class Smof implements Closeable {
 		collections.put(elClass, new SmofCollectionImpl<T>(collectionName, collection, elClass, parser, options));
 	}
 
-	public <T> void registerSmofObject(Class<T> type) throws SmofException {
+	/**
+	 * Registers an external (non-element) type in the API. This method is not strictly required, as all objects are lazy
+	 * registered as main types get used. Mind that all objects tagged with the {@link SmofObject} annotation must
+	 * have a {@link SmofBuilder} annotation.
+	 * 
+	 * @param type type to register
+	 */
+	public <T> void registerSmofObject(Class<T> type) {
 		parser.registerType(type);
 	}
 
-	public <T> void registerSmofFactory(Class<T> type, Object factory) throws SmofException {
+	/**
+	 * Registers an external (non-element) type in the API. This method is not strictly required, as all objects are lazy
+	 * registered as main types get used. However, this method is required if the {@link SmofBuilder} is placed at another 
+	 * type (e.g. factory pattern), as the lazy-register as no way of locating an external {@link SmofBuilder}. 
+	 * Mind that all objects tagged with the {@link SmofObject} annotation must have a {@link SmofBuilder} annotation.
+	 * 
+	 * The factory parameter can either be a an object or a class reference, if the {@link SmofBuilder} is static.
+	 * 
+	 * @param type type to register
+	 * @param factory object or class with the type's {@link SmofBuilder}
+	 */
+	public <T> void registerSmofFactory(Class<T> type, Object factory) {
 		parser.registerType(type, factory);
 	}
 
