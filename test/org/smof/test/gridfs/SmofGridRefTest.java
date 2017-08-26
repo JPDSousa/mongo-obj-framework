b29886bdfa733d3c807c83df9b8c59e6a07846b5
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-08-26 19:19:23 +0100

    tests for smof grid reference

---
+++ test/org/smof/test/gridfs/SmofGridRefTest.java
@@ -0,0 +1,72 @@
+package org.smof.test.gridfs;
+
+import static org.junit.Assert.*;
+
+import org.bson.types.ObjectId;
+import org.junit.Test;
+import org.smof.gridfs.SmofGridRef;
+import org.smof.gridfs.SmofGridRefFactory;
+import static org.smof.test.TestUtils.*;
+
+import java.nio.file.Path;
+
+@SuppressWarnings("javadoc")
+public class SmofGridRefTest {
+
+	private static final String BUCKET_1 = "bucket1";
+	private static final Path TEST_PATH = RESOURCES_4MB;
+	
+	@Test
+	public final void testCreate() {
+		assertNotNull(SmofGridRefFactory.newFromPath(TEST_PATH));
+		assertNotNull(SmofGridRefFactory.newFromFile(TEST_PATH.toFile()));
+		assertNotNull(SmofGridRefFactory.newFromDB(new ObjectId(), BUCKET_1));
+		assertNotNull(SmofGridRefFactory.newEmptyRef());
+	}
+	
+	@Test
+	public final void testNewFromDB() {
+		final ObjectId expectedID = new ObjectId();
+		final String expectedBucket = BUCKET_1;
+		final SmofGridRef ref = SmofGridRefFactory.newFromDB(expectedID, expectedBucket);
+		assertEquals(expectedID, ref.getId());
+		assertEquals(expectedBucket, ref.getBucketName());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public final void testNewFromDBNull() {
+		SmofGridRefFactory.newFromDB(new ObjectId(), null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public final void testNewFromDBNull1() {
+		SmofGridRefFactory.newFromDB(null, BUCKET_1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public final void testNewFromDBNull2() {
+		SmofGridRefFactory.newFromDB(null, null);
+	}
+	
+	@Test
+	public final void testAttachedFile() {
+		final SmofGridRef ref = SmofGridRefFactory.newFromPath(TEST_PATH);
+		assertEquals(TEST_PATH, ref.getAttachedFile());
+	}
+	
+	@Test
+	public final void testSetAttachedFile() {
+		final SmofGridRef ref = SmofGridRefFactory.newEmptyRef();
+		ref.attachFile(TEST_PATH);
+		assertEquals(TEST_PATH, ref.getAttachedFile());
+	}
+	
+	@Test
+	public final void testSetID() {
+		final SmofGridRef ref = SmofGridRefFactory.newEmptyRef();
+		final ObjectId expectedID = new ObjectId();
+		ref.setId(expectedID);
+		assertEquals(expectedID, ref.getId());
+	}
+
+}
