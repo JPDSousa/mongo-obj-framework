16b43f7421efb5f95b73fe88990b6fd2b952b5e6
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-05 08:51:15 +0100

    Added more test resources

--- test/org/smof/test/TestUtils.java
+++ test/org/smof/test/TestUtils.java
@@ -11,6 +11,8 @@ public class TestUtils {
 	public static final Path RESOURCES = Paths.get("testResources");
 	public static final Path RESOURCES_4MB = RESOURCES.resolve("test4mb.jpg");
 	public static final Path RESOURCES_1MB = RESOURCES.resolve("test1mb.jpg");
+	public static final Path RECOURCES_EL_GUITAR = RESOURCES.resolve("elecGuitar.jpg");
+	public static final Path RECOURCES_AC_GUITAR = RESOURCES.resolve("acGuitar.png");
 	
 	public static final String TEST_HOST = "localhost";
 	public static final String TEST_DB = "test";
