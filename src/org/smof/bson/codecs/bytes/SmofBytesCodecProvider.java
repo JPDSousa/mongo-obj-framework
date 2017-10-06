fc19216eb50860ecad4e825016667b67a7a6221b
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-06 15:24:41 +0100

    codec provider for SmofBytes

---
+++ src/org/smof/bson/codecs/bytes/SmofBytesCodecProvider.java
@@ -0,0 +1,37 @@
+package org.smof.bson.codecs.bytes;
+
+import java.util.Map;
+
+import org.bson.codecs.Codec;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.smof.bson.codecs.SmofCodecProvider;
+
+import com.google.common.collect.Maps;
+
+@SuppressWarnings("javadoc")
+public class SmofBytesCodecProvider implements SmofCodecProvider {
+
+	private final Map<Class<?>, Codec<?>> codecs;
+	
+	public SmofBytesCodecProvider() {
+		codecs = Maps.newLinkedHashMap();
+		put(new PrimitiveByteArrayCodec());
+		put(new GenericByteArrayCodec());
+	}
+	
+	private void put(Codec<?> codec) {
+		codecs.put(codec.getEncoderClass(), codec);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> Codec<T> get(Class<T> clazz, CodecRegistry registry) {
+		return (Codec<T>) codecs.get(clazz);
+	}
+
+	@Override
+	public boolean contains(Class<?> clazz) {
+		return codecs.containsKey(clazz);
+	}
+
+}
