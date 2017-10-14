c0f1c59f7ba534822b64b06e32fbfdaca45860e4
Mircea Cadariu <m.cadariu@sig.eu>
2017-10-14 19:40:08 +0200

    remove redundant visibility modifiers (interfaces methods are public my default)

--- src/org/smof/collection/SmofUpdate.java
+++ src/org/smof/collection/SmofUpdate.java
@@ -5,19 +5,19 @@ import org.smof.element.Element;
 
 public interface SmofUpdate<T extends Element> {
 
-    public SmofUpdate<T> setUpsert(boolean upsert);
+    SmofUpdate<T> setUpsert(boolean upsert);
 
-    public SmofUpdateQuery<T> where();
+    SmofUpdateQuery<T> where();
 
-    public void fromElement(T element);
+    void fromElement(T element);
 
-    public SmofUpdate<T> increase(Number value, String fieldName);
+    SmofUpdate<T> increase(Number value, String fieldName);
 
-    public SmofUpdate<T> multiply(Number value, String fieldName);
+    SmofUpdate<T> multiply(Number value, String fieldName);
 
-    public SmofUpdate<T> rename(String newName, String fieldName);
+    SmofUpdate<T> rename(String newName, String fieldName);
 
-    public SmofUpdate<T> set(BsonValue bson, String fieldName);
+    SmofUpdate<T> set(BsonValue bson, String fieldName);
 
-    public SmofUpdate<T> set(Object obj, String fieldName);
+    SmofUpdate<T> set(Object obj, String fieldName);
 }
