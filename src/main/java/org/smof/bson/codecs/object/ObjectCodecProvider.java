bc230a31fac45ad35eb07525260162711b807df1
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-24 10:48:27 +0100

    lazy loader and encoder context

--- src/main/java/org/smof/bson/codecs/object/ObjectCodecProvider.java
+++ src/main/java/org/smof/bson/codecs/object/ObjectCodecProvider.java
@@ -1,25 +1,27 @@
 package org.smof.bson.codecs.object;
 
-import java.util.Map;
-
 import org.bson.codecs.Codec;
+import org.bson.codecs.ObjectIdCodec;
 import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.types.ObjectId;
 import org.smof.bson.codecs.SmofCodecProvider;
-import org.smof.collection.SmofDispatcher;
 import org.smof.element.Element;
+import org.smof.field.MasterField;
+import org.smof.field.PrimaryField;
 import org.smof.field.SmofField;
-
-import com.google.common.collect.Maps;
+import org.smof.parsers.SmofParser;
 
 @SuppressWarnings("javadoc")
 public class ObjectCodecProvider implements SmofCodecProvider {
 	
 	private final LazyLoader lazyLoader;
 	private final ObjectCodecContext encoderContext;
+	private final SmofParser topParser;
 	
-	public ObjectCodecProvider(SmofDispatcher dispatcher) {
-		lazyLoader = LazyLoader.create(dispatcher);
+	public ObjectCodecProvider(SmofParser topParser) {
+		lazyLoader = LazyLoader.create(topParser.getDispatcher());
 		encoderContext = ObjectCodecContext.create();
+		this.topParser = topParser;
 	}
 	
 	@Override
@@ -32,17 +34,30 @@ public class ObjectCodecProvider implements SmofCodecProvider {
 	public <T> Codec<T> get(Class<T> type, CodecRegistry registry, SmofField field) {
 		if(field == null) {
 			//returns the default codec
-			return new ObjectCodec<>();
+			return new ObjectCodec<T>(type, encoderContext, topParser);
 		}
-		if(Element.class.isAssignableFrom(type)) {
-			return (Codec<T>) createElementReferenceCodec((Class<Element>) type, registry);
+		else if(field instanceof MasterField) {
+			return (Codec<T>) createMasterElementCodec((Class<? extends Element>) type, registry);
 		}
-		return null;
+		else if(Element.class.isAssignableFrom(type)) {
+			if(field instanceof PrimaryField) {
+				return
+			}
+			return (Codec<T>) createElementReferenceCodec((Class<? extends Element>) type, registry);
+		}
+		else if(ObjectId.class.isAssignableFrom(type)) {
+			return (Codec<T>) new ObjectIdCodec();
+		}
+		return new ObjectCodec<>(type, encoderContext, topParser);
+	}
+
+	private <T extends Element> MasterElementCodec<T> createMasterElementCodec(Class<T> type, CodecRegistry registry) {
+		return new MasterElementCodec<T>(type, registry, encoderContext);
 	}
 
 	@Override
 	public boolean contains(Class<?> clazz) {
-		return !clazz.isPrimitive() && !clazz.isArray()	&& !clazz.isEnum();
+		return !clazz.isPrimitive() && !clazz.isArray() && !clazz.isEnum();
 	}
 	
 	private <T extends Element> ReferenceElementCodec<T> createElementReferenceCodec(Class<T> type, CodecRegistry registry) {
