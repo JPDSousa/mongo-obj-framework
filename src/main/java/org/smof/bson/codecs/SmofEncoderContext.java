f411d2dc4def65ca00505e8deb36fbe639b3e362
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-20 10:56:18 +0000

    refactored

--- src/main/java/org/smof/bson/codecs/SmofEncoderContext.java
+++ src/main/java/org/smof/bson/codecs/SmofEncoderContext.java
@@ -1,18 +1,29 @@
 package org.smof.bson.codecs;
 
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 
-import org.bson.BsonElement;
+import org.smof.collection.SmofUpdate;
+import org.smof.element.Element;
 import org.smof.field.SmofField;
+
+import com.google.common.collect.Lists;
 
 @SuppressWarnings("javadoc")
 public class SmofEncoderContext {
 	
-	private final List<Callable<BsonElement>> posInsertionHooks;
+	public static SmofEncoderContext create(SmofField field) {
+		return new SmofEncoderContext(Lists.newArrayList(), field);
+	}
+	
+	public static SmofEncoderContext create(List<Consumer<SmofUpdate<Element>>> posInsertionHooks, SmofField field) {
+		return new SmofEncoderContext(posInsertionHooks, field);
+	}
+	
+	private final List<Consumer<SmofUpdate<Element>>> posInsertionHooks;
 	private final SmofField field;
 	
-	public SmofEncoderContext(List<Callable<BsonElement>> posInsertionHooks, SmofField field) {
+	private SmofEncoderContext(List<Consumer<SmofUpdate<Element>>> posInsertionHooks, SmofField field) {
 		super();
 		this.posInsertionHooks = posInsertionHooks;
 		this.field = field;
@@ -22,11 +33,11 @@ public class SmofEncoderContext {
 		return field;
 	}
 	
-	public void addPosInsertionHook(Callable<BsonElement> hook) {
+	public void addPosInsertionHook(Consumer<SmofUpdate<Element>> hook) {
 		posInsertionHooks.add(hook);
 	}
 	
-	public List<Callable<BsonElement>> getPosInsertionHooks() {
+	public List<Consumer<SmofUpdate<Element>>> getPosInsertionHooks() {
 		return posInsertionHooks;
 	}
 	
