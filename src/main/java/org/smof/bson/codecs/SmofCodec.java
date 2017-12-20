7aa28e7afc2b62205a730e8fc20bc478d5623fae
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-20 10:51:05 +0000

    additional deconding options

--- src/main/java/org/smof/bson/codecs/SmofCodec.java
+++ src/main/java/org/smof/bson/codecs/SmofCodec.java
@@ -1,13 +1,17 @@
 package org.smof.bson.codecs;
 
+import org.bson.BsonReader;
 import org.bson.BsonWriter;
 import org.bson.codecs.Codec;
+import org.bson.codecs.EncoderContext;
 
 @SuppressWarnings("javadoc")
 public interface SmofCodec<T> extends Codec<T> {
 	
+	EncoderContext DUMMY_CONTEXT = EncoderContext.builder().build();
+	
 	void encode(BsonWriter writer, T value, SmofEncoderContext context);
 	
-	
+	T decode(BsonReader reader, SmofEncoderContext context);
 
 }
