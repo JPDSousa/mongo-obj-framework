88f1397f6c825aa79499c9b89f2a21732be06206
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-20 10:51:26 +0000

    updated inheritance

--- src/main/java/org/smof/bson/codecs/SmofCollectibleCodec.java
+++ src/main/java/org/smof/bson/codecs/SmofCollectibleCodec.java
@@ -1,11 +1,10 @@
 package org.smof.bson.codecs;
 
-import org.bson.BsonWriter;
 import org.bson.codecs.CollectibleCodec;
 
 @SuppressWarnings("javadoc")
-public interface SmofCollectibleCodec<T> extends CollectibleCodec<T> {
-
-	void encode(BsonWriter writer, T value, SmofEncoderContext context);
+public interface SmofCollectibleCodec<T> extends CollectibleCodec<T>, SmofCodec<T> {
+	
+	// no additional methods required
 	
 }
