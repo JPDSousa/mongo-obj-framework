acd0dae6946c13c83fa6b48b2a528fe2e98f24d6
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-20 10:57:13 +0000

    added new codecs

--- src/main/java/org/smof/bson/codecs/object/SmofObjectCodecProvider.java
+++ src/main/java/org/smof/bson/codecs/object/SmofObjectCodecProvider.java
@@ -1,10 +1,14 @@
 package org.smof.bson.codecs.object;
 
+import java.util.Map;
+
+import org.bson.Document;
 import org.bson.codecs.Codec;
 import org.bson.codecs.ObjectIdCodec;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.types.ObjectId;
+import org.smof.bson.codecs.SmofCodec;
 import org.smof.element.Element;
 import org.smof.gridfs.SmofGridRef;
 import org.smof.parsers.SmofParser;
@@ -14,23 +18,39 @@ public class SmofObjectCodecProvider implements CodecProvider {
 
 	private final LazyLoader lazyLoader;
 	private final SmofParser topParser;
+	
+	private final SmofCodec<Map<String, Object>> mapCodec;
+	private final SmofCodec<Document> docCodec;
+	private final Codec<ObjectId> objIdCodec;
+	private final Codec<SmofGridRef> gridRefCodec;
 
 	public SmofObjectCodecProvider(SmofParser topParser) {
 		lazyLoader = LazyLoader.create(topParser.getDispatcher());
 		this.topParser = topParser;
+		this.mapCodec = new MapCodec(topParser);
+		this.docCodec = new DocumentCodec(mapCodec);
+		this.objIdCodec = new ObjectIdCodec();
+		this.gridRefCodec = new SmofGridRefCodec(topParser);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> Codec<T> get(Class<T> clazz, CodecRegistry registry) {
 		if(SmofGridRef.class.isAssignableFrom(clazz)) {
-			return (Codec<T>) new SmofGridRefCodec(topParser);
+			return (Codec<T>) gridRefCodec;
 		}
 		else if(Element.class.isAssignableFrom(clazz)) {
 			return (Codec<T>) createElementReferenceCodec((Class<? extends Element>) clazz);
 		}
+		else if(Map.class.isAssignableFrom(clazz)) {
+			// TODO check if Map<String, Object>
+			return (Codec<T>) mapCodec;
+		}
+		else if(Document.class.equals(clazz)) {
+			return (Codec<T>) docCodec;
+		}
 		else if(ObjectId.class.isAssignableFrom(clazz)) {
-			return (Codec<T>) new ObjectIdCodec();
+			return (Codec<T>) objIdCodec;
 		}
 		// returns the default codec
 		return new ObjectCodec<>(clazz, topParser);
