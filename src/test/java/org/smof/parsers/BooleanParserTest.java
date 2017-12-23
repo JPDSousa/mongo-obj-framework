2a45a70e893a0e9192a22dcad0c1885c22c13b1d
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-23 08:52:56 +0000

    using parser template

--- src/test/java/org/smof/parsers/BooleanParserTest.java
+++ src/test/java/org/smof/parsers/BooleanParserTest.java
@@ -29,24 +29,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import java.time.LocalDate;
-
-/**
- * Created by thales minussi on 28/09/17.
- * https://github.com/tminussi
- */
 @SuppressWarnings("javadoc")
-public class BooleanParserTest {
+public class BooleanParserTest implements ParserTest {
 
 	private static final BsonValue BSON_TRUE = BsonBoolean.TRUE;
 	private static final BsonValue BSON_FALSE = BsonBoolean.FALSE;
+	
 	private static BooleanParser parser;
 
 	@Before
-	public void setUp() {
-		parser = (BooleanParser) new SmofParser(null)
-				.getParsers()
-				.get(SmofType.BOOLEAN);
+	public final void setUp() {
+		parser = new BooleanParser(null, null);
 	}
 	
 	private void testSupport(Object valueTrue, Object valueFalse, Class<?> type) {
@@ -62,20 +55,23 @@ public class BooleanParserTest {
 	}
 
 	@Test
-	public void booleanSupport() {
-		testSupport(Boolean.TRUE, Boolean.FALSE, Boolean.class);
-		testSupport(true, false, boolean.class);
+	public final void testBoolean() {
+		assertParserResult(parser, BSON_TRUE, true);
+		assertParserResult(parser, BSON_FALSE, false);
+		assertParserResult(parser, BSON_TRUE, Boolean.TRUE);
+		assertParserResult(parser, BSON_FALSE, Boolean.FALSE);
 	}
 	
 	@Test
-	public void stringSupport() {
+	public final void stringSupport() {
 		final String valueTrue = Boolean.TRUE.toString();
 		final String valueFalse = Boolean.FALSE.toString();
-		testSupport(valueTrue, valueFalse, String.class);
+		assertParserResult(parser, BSON_TRUE, valueTrue);
+		assertParserResult(parser, BSON_FALSE, valueFalse);
 	}
 	
 	@Test
-	public void integerSupport() {
+	public final void integerSupport() {
 		final int valueTrue = 1;
 		final int valueFalse = 0;
 		testSupport(valueTrue, valueFalse, Integer.class);
@@ -83,40 +79,28 @@ public class BooleanParserTest {
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void fromBson_ShouldRaise_IllegalArgumentException_IfValueIsNull() {
+	public final void fromBson_ShouldRaise_IllegalArgumentException_IfValueIsNull() {
 		parser.fromBson(null, Boolean.class, null);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void fromBson_ShouldRaise_IllegalArgumentException_IfTypeIsNull() {
+	public final void fromBson_ShouldRaise_IllegalArgumentException_IfTypeIsNull() {
 		parser.fromBson(BSON_TRUE, null, null);
 	}
 
-	@Test
-	public void serializeToBson_ShouldSerialize_SmofFieldCorrectly() {
-		BsonValue bsonValue = parser.serializeToBson(Boolean.TRUE, null);
-		assertTrue(bsonValue.isBoolean());
-	}
-
-	@Test
-	public void serializeToBson_ShouldReturn_Null() {
-		BsonValue bsonValue = parser.serializeToBson(LocalDate.now(), null);
-		assertFalse(bsonValue.isBoolean());
-	}
-
 	@Test(expected = RuntimeException.class)
-	public void serializeToBson_ShouldRaise_RuntimeException_IfValueIsNull() {
-		parser.serializeToBson(null, null);
+	public final void serializeToBson_ShouldRaise_RuntimeException_IfValueIsNull() {
+		parser.toBson(null, null);
 	}
 
 	@Test
-	public void isValidBson_ShouldReturn_True() {
+	public final void isValidBson_ShouldReturn_True() {
 		assertTrue(parser.isValidBson(BSON_FALSE));
 		assertTrue(parser.isValidBson(BSON_TRUE));
 	}
 
 	@Test
-	public void isValidBson_ShouldReturn_False() {
+	public final void isValidBson_ShouldReturn_False() {
 		boolean validBson = parser.isValidBson(new BsonArray());
 		assertFalse(validBson);
 	}
